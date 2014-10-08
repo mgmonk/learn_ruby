d13ba8260d84eb02899b9ba25cae0b443b8df9ac
matt <mattmonk@gmail.com>
2014-10-08 10:18:01 -0400

    Complete pig latin implementation.

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -3,12 +3,15 @@ def translate(word)
   def pigify(word)
     if word.start_with?("a", "e", "i", "o", "u")
       word + 'ay'
-    elsif word.start_with?("qu")
-      word[2..word.length-1] + "qu" + "ay"
     else
       count = 0
       a = ''
       while (word[count] != 'a' && word[count] != 'e' && word[count] != 'i' && word[count] != 'o' && word[count] != 'u') do
+        if word.include?("qu")
+          place = word.index("qu")
+          a = word[0..place+1]
+          return word[place+2..word.length-1] + a + "ay"
+        end
         a += word[count]
         count += 1
       end
