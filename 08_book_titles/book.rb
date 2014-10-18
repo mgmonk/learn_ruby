5bc2a12b69a4069fc036dc13a0bc90896d5d5005
matt <mattmonk@gmail.com>
2014-10-18 15:54:04 -0400

    Complete book tests.

--- 08_book_titles/book.rb
+++ 08_book_titles/book.rb
@@ -5,7 +5,15 @@ class Book
   def title=(new_title)
     words = new_title.split(" ")
     words[0].capitalize!
-    
+    words[1..-1].map do |word|
+      small_words = %w{a the and in an of}
+      if small_words.include? word
+        word
+      else
+        word.capitalize!
+      end
+    end
+
     @title = words.join(" ")
   end
 
